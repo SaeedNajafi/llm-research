86ec5d5c2fec4b0e3ca084297952df1bfe6c00d7
snajafi <snajafi@v3.cluster.local>
2024-06-27 16:49:45 -0400

    Tested the working fsdp version.

--- src/data_utility.py
+++ src/data_utility.py
@@ -8,105 +8,15 @@ with other components (optimizer, model, etc.)
 
 import ast
 import random
-from typing import Dict, Iterator, List, Optional, Tuple
+from typing import List, Tuple
 
 import pandas as pd
-import torch
-import torch.distributed as dist
 from torch.utils.data import DataLoader
-from torch.utils.data.dataloader import _BaseDataLoaderIter
 from torch.utils.data.distributed import DistributedSampler
 
 from src.general_utils import DictDataset, white_space_fix
-from src.llama3 import Llama3
+from src.llama3 import LlamaQA
 from src.squadv2_llama3_instructions import explanation_icl_input, explanation_instruction, normal_icl_input, normal_instruction
-
-
-class DistributedSaveableSampler(DistributedSampler):
-    """Just like with the case with
-    torch.utils.data.distributed.DistributedSampler you *MUST* call
-    self.set_epoch(epoch:int) to ensure all replicates use the same random
-    shuffling within each epoch if shuffle is True."""
-
-    def __init__(
-        self,
-        dataset: torch.utils.data.dataset.Dataset,
-        num_replicas: Optional[int] = None,
-        rank: Optional[int] = None,
-        shuffle: bool = True,
-        seed: int = 0,
-        drop_last: bool = False,
-    ) -> None:
-        """
-        Arguments:
-            force_synchronization (boolean, optional): If it's true then after
-                each yield we will force a synchronization so each process'
-                _curr_idx will be the same, this guarantees correctness of the
-                save in case there is no synchronization during training, but
-                comes at a performance cost
-            For the rest of the arguments please see:
-                https://pytorch.org/docs/1.7.1/data.html?highlight=distributed%20sampler#torch.utils.data.distributed.DistributedSampler
-
-        """
-        super().__init__(dataset=dataset, num_replicas=num_replicas, rank=rank, shuffle=shuffle, seed=seed, drop_last=drop_last)
-
-        self._curr_idx = 0
-        self.force_synchronization = True
-
-    def __iter__(self) -> Iterator[int]:
-        """Logic modified from
-        https://pytorch.org/docs/1.7.1/_modules/torch/utils/data/distributed.html#DistributedSampler
-        """
-        if self.shuffle:
-            # deterministically shuffle based on epoch and seed
-            g = torch.Generator()
-            g.manual_seed(self.seed + self.epoch)
-            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore
-        else:
-            indices = list(range(len(self.dataset)))  # type: ignore
-
-        if not self.drop_last:
-            # add extra samples to make it evenly divisible
-            indices += indices[: (self.total_size - len(indices))]
-        else:
-            # remove tail of data to make it evenly divisible.
-            indices = indices[: self.total_size]
-        assert len(indices) == self.total_size
-
-        while self._curr_idx + self.rank < self.total_size:
-            to_yield = self.rank + self._curr_idx
-
-            # we need to increment this before the yield because
-            # there might be a save or preemption while we are yielding
-            # so we must increment it before to save the right index
-            self._curr_idx += self.num_replicas
-
-            yield to_yield
-
-            if self.force_synchronization:
-                dist.barrier()
-        self._curr_idx = 0
-
-    def state_dict(self, dataloader_iter: Optional[_BaseDataLoaderIter] = None) -> Dict[str, int]:
-        """Create a state dict for the DataSampler."""
-        prefetched_num = 0
-        # in the case of multiworker dataloader, the helper worker could be
-        # pre-fetching the data that is not consumed by the main dataloader.
-        # we need to subtract the unconsumed part .
-        if dataloader_iter is not None:
-            if dataloader_iter._num_workers > 0:
-                batch_size = dataloader_iter._index_sampler.batch_size
-                prefetched_num = (dataloader_iter._send_idx - dataloader_iter._rcvd_idx) * batch_size
-
-        return {
-            "index": self._curr_idx - (prefetched_num * self.num_replicas),
-            "epoch": self.epoch,
-        }
-
-    def load_state_dict(self, state_dict: Dict[str, int]) -> None:
-        """Load the DataSampler's state."""
-        self._curr_idx = state_dict["index"]
-        self.epoch = state_dict["epoch"]
 
 
 def process_squad_dataset(
@@ -178,7 +88,13 @@ def process_squad_dataset(
 
 
 def create_squadv2_dataloader(
-    model: Llama3, file_name: str, fold_name: str, experiment_type: str, batch_size: int = 1, world_size: int = 1, rank: int = 0
+    model: LlamaQA,
+    file_name: str,
+    fold_name: str,
+    experiment_type: str,
+    batch_size: int = 1,
+    world_size: int = 1,
+    rank: int = 0,
 ) -> DataLoader:
     """Function to create the required dataloader to train the LM models."""
 
@@ -200,7 +116,7 @@ def create_squadv2_dataloader(
         dataset,
         shuffle=False,
         batch_size=batch_size,
-        sampler=DistributedSaveableSampler(dataset, shuffle=shuffle, num_replicas=world_size, rank=rank),
+        sampler=DistributedSampler(dataset, shuffle=shuffle, num_replicas=world_size, rank=rank),
         num_workers=0,
     )
     return dataloader
