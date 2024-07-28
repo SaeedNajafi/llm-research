fc71950a141fa45be264cde53a47a8d961c53f8f
snajafi <snajafi@v3.cluster.local>
2024-07-28 13:43:40 -0400

    save the metric

--- src/metric_job.sh
+++ src/metric_job.sh
@@ -2,22 +2,16 @@
 
 set -x
 
-path="/scratch/ssd004/scratch/snajafi/gpt4-omini-predictions"
+path="/h/snajafi/final-gpt4-omini-squadv2"
 
 python src/metrics.py --metric_device=cuda:0 \
 --metric_type llm2vec \
---input_file ${path}/squadv2_original_validation_normal_no_icl.gtp4-omini.tsv >> ${path}/squadv2_original_validation_normal_no_icl.gtp4-omini.metrics.txt 2>&1
-
-'''
-python src/metrics.py --metric_device=cuda:0 \
---metric_type llm2vec \
---input_file ${path}/squadv2_predictions_original_validation.normal_icl.csv >> ${path}/normal_icl.metrics.txt 2>&1
+--input_file ${path}/squadv2_original_validation_normal_no_icl.gtp4-omini.tsv > ${path}/squadv2_original_validation_normal_no_icl.gtp4-omini.metrics.txt 2>&1
 
 python src/metrics.py --metric_device=cuda:0 \
 --metric_type llm2vec \
---input_file ${path}/squadv2_predictions_original_validation.normal_no_icl.csv >> ${path}/normal_no_icl.metrics.txt 2>&1
+--input_file ${path}/squadv2_original_validation_normal_with_icl.gtp4-omini.tsv > ${path}/squadv2_original_validation_normal_with_icl.gtp4-omini.metrics.txt 2>&1
 
 python src/metrics.py --metric_device=cuda:0 \
 --metric_type llm2vec \
---input_file ${path}/squadv2_predictions_original_validation.explanation_no_icl.csv >> ${path}/explanation_no_icl.metrics.txt 2>&1
-'''
+--input_file ${path}/squadv2_original_validation_explanation_no_icl.gtp4-omini.tsv > ${path}/squadv2_original_validation_explanation_no_icl.gtp4-omini.metrics.txt 2>&1
